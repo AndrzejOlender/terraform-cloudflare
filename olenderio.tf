49f99942126a8d5260d4f144f24a2ea402bf1500
Andrzej Olender <andrzej@olender.me>
2021-08-30 08:16:02 +0200

    add argocd domain

--- olenderio.tf
+++ olenderio.tf
@@ -16,6 +16,15 @@ resource "cloudflare_record" "A_www_olender_io_12ef522194807b2535cd212304977146"
   value   = "85.219.183.37"
 }
 
+resource "cloudflare_record" "A_argocd_olender_io_12ef522394407b2535cd212304977146" {
+  zone_id = "dab8f099a595da90a93887a6b1984eb5"
+  name    = "argocd"
+  type    = "A"
+  ttl     = "1"
+  proxied = "false"
+  value   = "85.219.183.37"
+}
+
 resource "cloudflare_record" "MX_olender_io_8ede0bb9917ae57706b25a442d7c48f4" {
   zone_id  = "dab8f099a595da90a93887a6b1984eb5"
   name     = "olender.io"
