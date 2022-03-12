51b916d25a199b786682587e501d04d45b8229f8
Andrzej Olender <andrzej.olender@esky.com>
2022-03-12 21:36:45 +0100

    add record MX

--- sipbpl.tf
+++ sipbpl.tf
@@ -16,3 +16,39 @@ resource "cloudflare_record" "A_www_sipb_pl_eda5719077aa3c1530b7d12df2c8e642" {
   proxied = "true"
   value   = "85.219.183.37"
 }
+
+resource "cloudflare_record" "A_www_sipb_pl_eda5719077aa3c1530b7d12df2c8e642" {
+  zone_id = "66a77f09e1ba43a2f4a496cfcf33c6d7"
+  name    = "sipb.pl"
+  type    = "MX"
+  ttl     = "1"
+  proxied = "false"
+  value   = "route1.mx.cloudflare.net	"
+}
+
+resource "cloudflare_record" "A_www_sipb_pl_eda5719077aa3c1530b7d12d22c8e642" {
+  zone_id = "66a77f09e1ba43a2f4a496cfcf33c6d7"
+  name    = "sipb.pl"
+  type    = "MX"
+  ttl     = "1"
+  proxied = "false"
+  value   = "route2.mx.cloudflare.net"
+}
+
+resource "cloudflare_record" "A_www_sipb_pl_eda5719077aa3c1330b7d12df2c8e642" {
+  zone_id = "66a77f09e1ba43a2f4a496cfcf33c6d7"
+  name    = "sipb.pl"
+  type    = "MX"
+  ttl     = "1"
+  proxied = "false"
+  value   = "route3.mx.cloudflare.net"
+}
+
+resource "cloudflare_record" "A_www_sipb_pl_eda5719075aa3c1530b7d12df2c8e642" {
+  zone_id = "66a77f09e1ba43a2f4a496cfcf33c6d7"
+  name    = "www"
+  type    = "TXT"
+  ttl     = "1"
+  proxied = "false"
+  value   = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+}
