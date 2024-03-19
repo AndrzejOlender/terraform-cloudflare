cf2383cf9852df2f02c48952992fdd10534e6a13
Andrzej Olender <andrzej@olender.me>
2024-03-19 16:12:03 +0100

    add new domain

---
+++ theshineshoes.tf
@@ -0,0 +1,46 @@
+resource "cloudflare_record" "A_theshine_shoes_3241deb5a27491a5f5756a25d091e39b" {
+  zone_id = "cc48edd90525ea1c42435ecc79d83b66"
+  name    = "theshine_shoes"
+  type    = "A"
+  ttl     = "1"
+  proxied = "true"
+  value   = "176.97.53.34"
+}
+
+resource "cloudflare_record" "A_www_theshine_shoes_12ef522194807b2535cd212304977146" {
+  zone_id = "cc48edd90525ea1c42435ecc79d83b66"
+  name    = "www"
+  type    = "A"
+  ttl     = "1"
+  proxied = "true"
+  value   = "176.97.53.34"
+}
+
+resource "cloudflare_record" "MX_theshine_shoes_8ede0bb9917ae57706b25a442d7c48f4" {
+  zone_id  = "cc48edd90525ea1c42435ecc79d83b66"
+  name     = "theshine_shoes"
+  type     = "MX"
+  ttl      = "1"
+  proxied  = "false"
+  priority = "1"
+  value    = "aspmx.l.google.com"
+}
+
+resource "cloudflare_record" "TXT_theshine_shoes_7731ed366b8b3c4b85f999bce473556e" {
+  zone_id = "cc48edd90525ea1c42435ecc79d83b66"
+  name    = "theshine_shoes"
+  type    = "TXT"
+  ttl     = "1"
+  proxied = "false"
+  value   = "v=spf1 mx a include:_spf.emaillabs.net.pl ~all"
+}
+
+resource "cloudflare_record" "TXT_theshine_shoes_7731ed366b8b3c4b85f999bc4473556e" {
+  zone_id = "cc48edd90525ea1c42435ecc79d83b66"
+  name    = "_acme-challenge.dns.theshine_shoes"
+  type    = "TXT"
+  ttl     = "1"
+  proxied = "false"
+  value   = "CGiRLq9acUSOwgRsE1pR_oUJMghulYs86Z4Xb1LSceo"
+}
+
